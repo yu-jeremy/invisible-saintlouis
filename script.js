9e63bb89cbe72c9ceb0caea7b4ac18bd3c95a42e
Jeremy Yu <jeremy.yu@wustl.edu>
2019-04-16 17:26:06 -0500

    Editing styles, added title and footer

--- script.js
+++ script.js
@@ -4,29 +4,12 @@ window.addEventListener('DOMContentLoaded', function () {
 
     AOS.init();
     var containers = document.getElementsByClassName("container");
-    var timeline_boxes = document.getElementsByClassName("content");
     var timeline_2000 = document.getElementById("hidden_2000");
     var explore_2000 = document.getElementById("explore_2000");
 
     Array.from(containers).forEach(
         function (element) {
-            element.setAttribute("data-aos-duration", "1000")
-        }
-    );
-
-    Array.from(timeline_boxes).forEach(
-        function (element) {
-            element.addEventListener("mouseover", function () {
-                $(this).css("transform", "scale(1.04)")
-            });
-        }
-    );
-
-    Array.from(timeline_boxes).forEach(
-        function (element) {
-            element.addEventListener("mouseout", function () {
-                $(this).css("transform", "scale(1.0)")
-            });
+            element.setAttribute("data-aos-duration", "1250")
         }
     );
 
