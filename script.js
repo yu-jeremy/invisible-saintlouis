144a3f77c6ffd42a2f2a7f04eb668c940eafa2b2
Jeremy Yu <jeremy.yu@wustl.edu>
2019-04-16 15:19:09 -0500

    Adding more events to timeline, also added header menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 window.addEventListener('DOMContentLoaded', function () {
     var timeline_boxes = document.getElementsByClassName("timeline_description");
+    var timeline_2000 = document.getElementById("hidden_2000");
+    var explore_2000 = document.getElementById("explore_2000");
 
     Array.from(timeline_boxes).forEach(
         function (element) {
@@ -9,5 +11,22 @@ window.addEventListener('DOMContentLoaded', function () {
             });
         }
     );
+
+    explore_2000.addEventListener("click", function () {
+        if (explore_2000.textContent == "Explore") {
+            timeline_2000.removeAttribute("hidden");
+            explore_2000.textContent = "See Less";
+            $(explore_2000).css("background-color", "red");
+        } else if (explore_2000.textContent = "See Less") {
+            timeline_2000.hidden = true;
+            explore_2000.textContent = "Explore";
+            $(explore_2000).css("background-color", "#274690");
+        }
+        
+        
+       
+    });
+
+
     
 });
