3c19d8a5afdf9adc29cbd93cc577491e891b2b23
Jeremy Yu <jeremy.yu@wustl.edu>
2019-04-29 10:57:32 -0500

    Editing broader timeline
    Still need CSS changes for when we situate EMASS timeline within larger timeline (containers don't have lighter color)

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
     AOS.init();
     var containers = document.getElementsByClassName("container");
+
     var timeline_2000 = document.getElementById("hidden_2000");
     var explore_2000 = document.getElementById("explore_2000");
+
+    var timeline_2019 = document.getElementsByClassName("hidden_2019");
+    var explore_2019 = document.getElementById("explore_2019");
+    var quote_2019 = document.getElementById("quote_2019");
+
     var buttons = document.getElementsByClassName("pure-button");
     var situate_btn = document.getElementById("situate_button");
     var emass_timeline = document.getElementById("emass_timeline");
@@ -80,4 +86,26 @@ window.addEventListener('DOMContentLoaded', function () {
             $(explore_2000).css("background-color", "#284B63");
         }
     });
+
+    explore_2019.addEventListener("click", function () {
+        if (explore_2019.textContent == "Explore") {
+            Array.from(timeline_2019).forEach(
+                function (element) {
+                    element.removeAttribute("hidden");
+                }
+            );
+            quote_2019.removeAttribute("hidden");
+            explore_2019.textContent = "See Less";
+            $(explore_2019).css("background-color", "red");
+        } else if (explore_2019.textContent = "See Less") {
+            Array.from(timeline_2019).forEach(
+                function (element) {
+                    element.hidden = true;
+                }
+            );
+            quote_2019.hidden = true;
+            explore_2019.textContent = "Explore";
+            $(explore_2019).css("background-color", "#284B63");
+        }
+    })
 });
